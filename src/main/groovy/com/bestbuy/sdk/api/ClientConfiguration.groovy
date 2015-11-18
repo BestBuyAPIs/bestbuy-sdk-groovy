45934937ef439715cd29d335be82c1a948bec83e
machador <machador@gmail.com>
2015-11-18 11:37:45 -0300

    Moved library configuration to external properties file

--- src/main/groovy/com/bestbuy/sdk/api/ClientConfiguration.groovy
+++ src/main/groovy/com/bestbuy/sdk/api/ClientConfiguration.groovy
@@ -1,20 +1,33 @@
 package com.bestbuy.sdk.api
 
-/**
- * Created by tarmac on 11/3/15.
- */
+import groovy.util.logging.Log4j
 
+@Log4j
 class ClientConfiguration {
 
+    static private Properties conf
+    static {
+        conf = new Properties()
+        try {
+            new File(ClassLoader.getSystemResource('bestbuy-sdk-groovy.properties').file).withInputStream {
+                stream -> conf.load(stream)
+            }
+            log.debug "Configuration properties loaded correctly"
+        } catch (Exception e) {
+            log.error "Configuration properties loading failed: $e.message"
+            throw e
+        }
+    }
+
     static String getDefaultApiKey() {
-        System.getenv('BBY_API_KEY')
+        System.getenv(conf.getProperty('bestbuy.api.keyEnvName'))
     }
 
     static String getBaseUrl() {
-        'http://api.remix.bestbuy.com/v1/'
+        conf.getProperty('bestbuy.api.baseUrl')
     }
 
     static String getBaseBetaUrl() {
-        'http://api.bestbuy.com/beta/'
+        conf.getProperty('bestbuy.api.betaBaseUrl')
     }
 }
